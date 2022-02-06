cc34a1623ef082885880cac8ebe33a07c99c8fff
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-06 22:45:11 +0530

    Passing All HTTP request for Greeting Message

---
+++ src/main/java/com/bridgelabz/greetingapp/entity/User.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.greetingapp.entity;
+
+public class User {
+    private  String firstName;
+    private  String lastName;
+
+    public User(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
